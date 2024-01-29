7a560257b0dfb18d91cfa27466b171ca2a3970c6
LordW01 <158026515+LordW01@users.noreply.github.com>
2024-01-29 10:36:18 +0300

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: "Botunuzun Tokenini Giriniz",
-    id: "Botunuzun ID'sini Giriniz"
+    token: MTIwMDgwNTQ1NjA4MDI4OTg3Mw.GNZohY.4qkCrAj59y67cHmIafpCsIu0h8byluCxCudgw8
+    id: 1200805456080289873
 }
